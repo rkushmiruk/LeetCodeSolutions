873016b52c9f3d8c90596e0e4ffe0017b6567323
rkushmiruk <rKushmiruk@gmail.com>
2020-10-06 21:17:54 +0300

    Add solutions

--- src/main/java/com/kushmyruk/CodeWarsSolutions/level7ku/DivisorsOfANumber.java
+++ src/main/java/com/kushmyruk/CodeWarsSolutions/level7ku/DivisorsOfANumber.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
  */
 public class DivisorsOfANumber {
     public static long numberOfDivisors(int n) {
-        return IntStream.range(1, n+1).filter(i -> n%i==0).count();
+        return IntStream.range(1, n + 1).filter(i -> n % i == 0).count();
     }
 
 //    public static long numberOfDivisors(int n) {
